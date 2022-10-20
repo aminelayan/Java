c0c0e49c30cf9fada14cd3b1017c9e4d3d937106
Amin Elayan <ameen.m.elayyan@gmail.com>
2022-10-20 14:42:05 +0300

    First Java Program

---
+++ Assginments/First Java Program/Test.java
@@ -0,0 +1,11 @@
+public class Test {
+	public static void main(String[] args) {
+        System.out.println("My Name is Amin");
+        System.out.println("I am 23 Years Old");
+        System.out.println("My HomeTown in Bethlehem, Battir");
+	}
+}
+
+
+
+
