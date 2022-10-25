309401e096cd944ab73bb03be090a6fac397e9ff
Amin Elayan <ameen.m.elayyan@gmail.com>
2022-10-25 14:14:22 +0300

    update

--- Assginments/Map of the Hashmatique/src/HashMapFun.java
+++ Assginments/Map of the Hashmatique/src/HashMapFun.java
@@ -4,13 +4,12 @@ import java.util.HashMap;
 public class HashMapFun {
     public static void main(String[] args) {
         HashMap<String, String> userMap = new HashMap<String, String>();
-        userMap.put("nninja@codingdojo.com", "Nancy Ninja");
-        userMap.put("ssamurai@codingdojo.com", "Sam Samurai");
-        userMap.put("wwizard@codingdojo.com", "Walter Wizard");
+        userMap.put("Track 1 Title", "Track 1 Lyrics");
+        userMap.put("Track 2 Title", "Track 2 Lyrics");
+        userMap.put("Track 3 Title", "Track 3 Lyrics");
         Set<String> keys = userMap.keySet();
         for(String key : keys) {
-            System.out.println(key);
-            System.out.println(userMap.get(key));
+            System.out.println("Track: "+ key + " | Lyrics: "+ userMap.get(key));
         }
     }
 }
